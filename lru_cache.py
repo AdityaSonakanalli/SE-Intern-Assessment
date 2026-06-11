b8939526f5e76885eefeacb2e9c7e1b06541cd47
AdityaSonakanalli <adityasonakanalli123@gmail.com>
2026-06-11 10:14:48 +0530

    Add files via upload

---
+++ lru_cache.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, key=0, value=0):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+
+        # dummy head and tail to avoid edge cases
+        self.head = Node()
+        self.tail = Node()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node):
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+    def _insert_at_front(self, node):
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+        self._remove(node)
+        self._insert_at_front(node)  # mark as most recently used
+        return node.value
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node = self.cache[key]
+            node.value = value
+            self._remove(node)
+            self._insert_at_front(node)
+            return
+
+        if len(self.cache) >= self.capacity:
+            lru = self.tail.prev   # LRU is just before dummy tail
+            self._remove(lru)
+            del self.cache[lru.key]
+
+        new_node = Node(key, value)
+        self.cache[key] = new_node
+        self._insert_at_front(new_node)
